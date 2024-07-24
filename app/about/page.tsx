548a9fd1977e35db55d2dc785eb06ac4e26f59ec
thatGui <thatguifonseca@gmail.com>
2024-07-24 11:01:50 +0100

    metadata + cleanup

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+	title: 'theNorm',
+	description: 'Your own personal Jo in your pocket',
+};
 
 export default function About() {
 	return (
