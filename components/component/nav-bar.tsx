50199d4b96f17f79dd49c197cc113ac5a1604606
Fearghal Kavanagh <121862261+Feargh@users.noreply.github.com>
2024-07-24 16:19:38 +0100

    Linked the pages to the link components
    Relates #6

--- components/component/nav-bar.tsx
+++ components/component/nav-bar.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export function NavBar() {
 	return (
 		<header className="flex h-16 items-center justify-between px-4 md:px-6">
-			<Link href="#" className="flex items-center gap-2" prefetch={false}>
+			<Link href="/" className="flex items-center gap-2" prefetch={false}>
 				<Image
 					src="/The-Norm_Logo_Strapline_ORANGE_1.svg"
 					width={100}
@@ -15,28 +15,28 @@ export function NavBar() {
 			<nav className="bg-white p-4">
 				<div className="flex">
 					<Link
-						href="#"
+						href="/about"
 						className="bg-orange-200 text-white px-4 py-2 rounded-l-lg hover:bg-black-600 hover:bg-orange-300"
 						prefetch={false}
 					>
 						About
 					</Link>
 					<Link
-						href="#"
+						href="/account"
 						className="bg-orange-200 text-white px-4 py-2 hover:bg-black-600 hover:bg-orange-300"
 						prefetch={false}
 					>
 						Account
 					</Link>
 					<Link
-						href="#"
+						href="/chat"
 						className="bg-orange-200 text-white px-4 py-2 hover:bg-black-600 hover:bg-orange-300"
 						prefetch={false}
 					>
 						Chat
 					</Link>
 					<Link
-						href="#"
+						href="/signUpLogin"
 						className="bg-orange-200 text-white px-4 py-2 rounded-r-lg hover:bg-black-600 hover:bg-orange-300"
 						prefetch={false}
 					>
