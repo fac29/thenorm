c1c80b7027af6ee3f589c9dee817a6a869776a79
Fearghal Kavanagh <121862261+Feargh@users.noreply.github.com>
2024-07-24 17:42:42 +0100

    Changed colour to white and made it bigger
    Relates #6

--- components/component/nav-bar.tsx
+++ components/component/nav-bar.tsx
@@ -3,41 +3,41 @@ import Image from 'next/image';
 
 export function NavBar() {
 	return (
-		<header className="flex h-16 items-center justify-between px-4 md:px-6">
+		<header className="flex h-32 items-center justify-between px-4 md:px-6">
 			<Link href="/" className="flex items-center gap-2" prefetch={false}>
 				<Image
-					src="/The-Norm_Logo_Strapline_ORANGE_1.svg"
-					width={100}
-					height={100}
+					src="/The-Norm_Logo_Strapline_WHITE_1.svg"
+					width={200}
+					height={200}
 					alt="The Norm Logo"
 				/>
 			</Link>
-			<nav className="bg-white p-4">
+			<nav className="p-4">
 				<div className="flex">
 					<Link
 						href="/about"
-						className="bg-orange-200 text-white px-4 py-2 rounded-l-lg hover:bg-black-600 hover:bg-orange-300"
+						className="bg-white text-black px-8 py-4 rounded-l-lg hover:bg-black-600 hover:bg-#e6e6e6"
 						prefetch={false}
 					>
 						About
 					</Link>
 					<Link
 						href="/account"
-						className="bg-orange-200 text-white px-4 py-2 hover:bg-black-600 hover:bg-orange-300"
+						className="bg-white text-black px-8 py-4 hover:bg-black-600 hover:bg-#e6e6e6"
 						prefetch={false}
 					>
 						Account
 					</Link>
 					<Link
 						href="/chat"
-						className="bg-orange-200 text-white px-4 py-2 hover:bg-black-600 hover:bg-orange-300"
+						className="bg-white text-black px-8 py-4 hover:bg-black-600 hover:bg-#e6e6e6"
 						prefetch={false}
 					>
 						Chat
 					</Link>
 					<Link
 						href="/signUpLogin"
-						className="bg-orange-200 text-white px-4 py-2 rounded-r-lg hover:bg-black-600 hover:bg-orange-300"
+						className="bg-white text-black px-8 py-4 rounded-r-lg hover:bg-black-600 hover:bg-#e6e6e6"
 						prefetch={false}
 					>
 						Login
