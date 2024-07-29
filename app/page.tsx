f9e496d04042a5e496919825b95f5e06f4384f13
Fearghal Kavanagh <121862261+Feargh@users.noreply.github.com>
2024-07-29 15:05:51 +0100

    Pretier formating, added image and titles

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,35 @@
-import { ModeToggle } from '@/components/modetoggle';
-import { Button } from '@/components/ui/button';
+import { ModeToggle } from "@/components/modetoggle";
+import { Button } from "@/components/ui/button";
 import {
 	Accordion,
 	AccordionContent,
 	AccordionItem,
 	AccordionTrigger,
-} from '@/components/ui/accordion';
+} from "@/components/ui/accordion";
 
 //import Image from 'next/image';
 
 export default function Home() {
 	return (
-		<div className='flex min-h-screen flex-col items-center justify-between p-24'>
-			<div>
+		<div className="flex min-h-screen flex-col items-center justify-between">
+			<img
+				className="p-4 m-2 w-1/2 w-500 transition-transform duration-1000 hover:scale-105 hover:brightness-105"
+				src="The-Norm_Wheel_MAIN_All-Colours_1.svg"
+				alt=""
+			/>
+			<h1 className="text-3xl font-bold mb-8 text-gray-800">the norm</h1>
+			<h2>find and nourish yours now</h2>
+			{/* <img src="The-Norm_Wheel_WHITE 1.jpg" alt="" /> */}
+
+			{/* <div>
 				<ModeToggle />
+			</div> */}
+			<div>
+				<Button variant="default">Start now..</Button>
 			</div>
 			<div>
-				<Button variant='default'>Hello</Button>
-			</div>
-			<div>
-				<Accordion type='single' collapsible>
-					<AccordionItem value='item-1'>
+				<Accordion type="single" collapsible>
+					<AccordionItem value="item-1">
 						<AccordionTrigger>Is it accessible?</AccordionTrigger>
 						<AccordionContent>
 							Yes. It adheres to the WAI-ARIA design pattern.
