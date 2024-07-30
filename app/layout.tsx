aff6e444b950ba43b4415e2f14ae7713cb06e1a5
Fearghal Kavanagh <121862261+Feargh@users.noreply.github.com>
2024-07-30 17:33:32 +0100

    Changed navbar import

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/themeprovider";
-import { NavBar } from "@/components/component/nav-bar";
+import { NavBar } from "@/components/navbar";
 import { Footer } from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
