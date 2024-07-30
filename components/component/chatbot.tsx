050460f1d42e8be28b97d8908f6dcb39aff0ae06
Fearghal Kavanagh <121862261+Feargh@users.noreply.github.com>
2024-07-30 17:00:11 +0100

    deleted commented out button

--- components/component/chatbot.tsx
+++ components/component/chatbot.tsx
@@ -63,12 +63,6 @@ const ChatBot: React.FC = () => {
 					>
 						Send
 					</Button>
-					{/* <button
-						onClick={handleSend}
-						className="bg-purple-500 text-white px-4 py-2 rounded-lg"
-					>
-						Send
-					</button> */}
 				</div>
 			</div>
 		</div>
