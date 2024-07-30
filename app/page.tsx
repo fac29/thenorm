b44d815002453572511b175f842af94f37925dad
Fearghal Kavanagh <121862261+Feargh@users.noreply.github.com>
2024-07-30 12:38:44 +0100

    Changed initial wheel on the home page to the white version

--- app/page.tsx
+++ app/page.tsx
@@ -54,8 +54,8 @@ export default function Home() {
 		<div className="flex min-h-screen flex-col items-center justify-between">
 			<img
 				className="p-4 m-2 w-1/2 w-500 transition-transform duration-1000 hover:scale-105 hover:brightness-105"
-				src="The-Norm_Wheel_MAIN_All-Colours_1.svg"
-				alt=""
+				src="The-Norm_Wheel_WHITE.svg"
+				alt="The normal wheel which in white. When you complete the quiz you will see this with colours representing your score"
 			/>
 			<h1 className="text-3xl font-bold mb-8 text-gray-800">the norm</h1>
 			<h2>find and nourish yours now</h2>
