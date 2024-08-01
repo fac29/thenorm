274a656b0680bd4f10eb757cee84eff3d274f7b7
Alex <alexperezdavies@gmail.com>
2024-08-01 11:59:29 +0100

    create landing page component for logged in and form complete

---
+++ components/LandingPageLoggedInTypeFormComplete.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+
+import PieChart from "@/components/PieChart";
+
+interface LandingPageProps {
+	completedWheel: boolean;
+}
+
+export default function LandingPageLoggedInTypeFormComplete({
+	completedWheel,
+}: LandingPageProps) {
+	const segmentNames = [
+		"Identify: somewhat",
+		"Lifespan: somewhat",
+		"Community: not at all",
+		"Passions: somewhat",
+		"Structure: somewhat",
+		"Pressure points: somewhat",
+		"Development: well",
+		"Support: somewhat",
+		"The Barrel: somewhat",
+		"Transition Management: well",
+		"Strengths: well",
+		"Balance: somewhat",
+		"Energy flow: not at all",
+		"Emotions: not at all",
+		"Gut health: somewhat",
+		"Body: not at all",
+		"Senses: somewhat",
+		"Brain: well",
+	];
+	return (
+		<div className="flex flex-col items-center justify-center px-4 sm:px-6 lg:px-8 py-8">
+			{" "}
+			<div className="w-full max-w-6xl flex flex-col lg:flex-row items-center lg:items-start space-y-8 lg:space-y-0 lg:space-x-12">
+				<div className="w-full lg:w-1/2 flex justify-center items-center">
+					<PieChart
+						segmentNames={segmentNames}
+						completedWheel={completedWheel}
+					/>
+				</div>
+				<div className="w-full lg:w-1/2 flex flex-col justify-center items-center lg:items-start space-y-4">
+					<h1 className="text-3xl font-bold text-gray-800 text-center lg:text-left">
+						the norm
+					</h1>
+					<h2 className="text-xl text-gray-600 text-center lg:text-left">
+						find and nourish yours now
+					</h2>
+					<Button variant="default">Start now..</Button>
+				</div>
+			</div>
+		</div>
+	);
+}
