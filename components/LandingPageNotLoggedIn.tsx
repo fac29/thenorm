f47c34328987c182e6cbce935df8e5c5f5f12f60
Alex <alexperezdavies@gmail.com>
2024-08-05 14:57:01 +0100

    change way circle is shown if logged in or not

---
+++ components/LandingPageNotLoggedIn.tsx
@@ -0,0 +1,46 @@
+import { Button } from "@/components/ui/button";
+
+import PieChart from "@/components/PieChart";
+
+export default function LandingPageNotLoggedIn() {
+	const segmentNames = [
+		"Identify",
+		"Lifespan",
+		"Community",
+		"Passions",
+		"Structure",
+		"Pressure points",
+		"Development",
+		"Support",
+		"The Barrel",
+		"Transition Management",
+		"Strengths",
+		"Balance",
+		"Energy flow",
+		"Emotions",
+		"Gut health",
+		"Body",
+		"Senses",
+		"Brain",
+	];
+	return (
+		<div className="flex max-h-screen flex-col items-center justify-center px-4 sm:px-6 lg:px-8 mb-8 md:mb-12 lg:mb-16 xl:mb-24">
+			<div className="w-full max-w-6xl flex flex-col lg:flex-row items-center justify-center space-y-8 lg:space-y-0 lg:space-x-12">
+				<div className="w-full lg:w-1/2 flex justify-center items-center">
+					<PieChart segmentNames={segmentNames} />
+				</div>
+				<div className="w-full lg:w-1/2 flex flex-col justify-center items-center lg:items-start space-y-4">
+					<h1 className="text-3xl font-bold text-gray-800 text-center lg:text-left">
+						the norm
+					</h1>
+					<h2 className="text-xl text-gray-600 text-center lg:text-left">
+						find and nourish yours now
+					</h2>
+					<Button variant="default">
+						<a href="/api/auth/login">Start now...</a>
+					</Button>
+				</div>
+			</div>
+		</div>
+	);
+}
