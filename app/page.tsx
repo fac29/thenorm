4d0823443ca8ce3e599d3c1ce260cce9558eff5c
Alex <alexperezdavies@gmail.com>
2024-08-05 14:58:02 +0100

    get user from session and render page whether user exists

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,17 @@
-"use client";
-
-import { useState } from "react";
+import { getSession } from "@auth0/nextjs-auth0";
 
 import LandingPageNotLoggedIn from "../components/LandingPageNotLoggedIn";
 import LandingPageLoggedIn from "../components/LandingPageLoggedIn";
 import FAQ from "@/components/FAQ";
 
-export default function Home() {
-	const [isLoggedIn, setIsLoggedIn] = useState<boolean>(true);
-	const [completedWheel, setCompletedWheel] = useState<boolean>(false);
+export default async function Home() {
+	const session = await getSession();
+	const user = session?.user;
+
 	return (
 		<div>
-			{isLoggedIn === false && completedWheel === false && (
-				<LandingPageTypeformNotComplete completedWheel={completedWheel} />
-			)}
-			{isLoggedIn === true && completedWheel === false && (
-				<LandingPageTypeformNotComplete completedWheel={completedWheel} />
-			)}
-			{isLoggedIn === true && completedWheel === true && (
-				<LandingPageLoggedInTypeFormComplete completedWheel={completedWheel} />
-			)}
+			{user === undefined && <LandingPageNotLoggedIn />}
+			{user && <LandingPageLoggedIn />}
 			<FAQ />
 		</div>
 	);
